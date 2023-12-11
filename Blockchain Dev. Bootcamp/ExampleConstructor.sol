bfbae3f1d0c2ab870f83c50305c95d12799d61ed
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2023-12-11 05:35:42 -0800

    Showed an example of how to use a constructor and its behavious

--- Blockchain Dev. Bootcamp/ExampleConstructor.sol
+++ Blockchain Dev. Bootcamp/ExampleConstructor.sol
@@ -1,3 +1,25 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.15;
+
+
+contract ExampleConstructor {
+
+     address public myAddress;
+
+     // A constructor is a  special function that does not carry the function keyword:
+     // It has no visibility specifier
+     // The special thing is that it is DEIRECTLY CALLED ONCE during depolyment of a smart contract. It cannot be overloaded.
+     // You cannot NOT call it.
+     constructor(address _someAddress) { 
+        myAddress = _someAddress;
+     }
+
+     function setMyAddress(address _myAddress) public {
+        myAddress = _myAddress;
+     }
+
+    function setMyAddressToMsgSender() public {
+        myAddress = msg.sender;
+    }
+}   
