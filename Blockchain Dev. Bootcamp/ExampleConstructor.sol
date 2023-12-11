aa702285b722f4d256e3bebd82126b14bbce9f7e
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2023-12-11 05:37:33 -0800

    Added notes explaining the constructor function

--- Blockchain Dev. Bootcamp/ExampleConstructor.sol
+++ Blockchain Dev. Bootcamp/ExampleConstructor.sol
@@ -2,6 +2,17 @@
 
 pragma solidity 0.8.15;
 
+/*
+
+The constructor is a special function. It is automatically called during Smart Contract deployment. And it can never be called again after that.
+
+It also has a special name! It's simply called constructor() { ... }.
+
+Let's see how that works to our advantage. Let's extend the Smart Contract we wrote before to make it a bit more secure.
+
+constructor(): is a special function that is called only once during contract deployment. It can have arguments, like here. But it also has the same global objects available as in any other transaction. So in msg.sender is the address of the person who deployed the Smart Contract, which you can use.
+
+*/
 
 contract ExampleConstructor {
 
