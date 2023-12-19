0f0da26d25c51b21a10ddafa18ee260627c92d58
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2023-12-19 05:42:48 -0800

    an example of a receive function

--- Blockchain Dev. Bootcamp/Projects/SampleFallback.sol
+++ Blockchain Dev. Bootcamp/Projects/SampleFallback.sol
@@ -6,5 +6,11 @@ contract SampleFallback {
 
     uint public lastValueSent;
 
-
+// a Receive function only relies on 2300gas (gas stipend) - which is very low.
+// If another contract interacts with this contract, nothing meaningful can really happen due to the low gas amount
+    // i.e, if there is no data to send, and only a value transfer without any data attached to the smart contract - it automatically hits the receive function
+    receive() external payable {
+        lastValueSent = msg.value;
+        lastFunctionCalled = "receive";
+    }
 }
