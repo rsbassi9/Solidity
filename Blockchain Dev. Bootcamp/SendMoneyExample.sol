6971fce5716a9413e892849d1e2466e506fb4c60
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2023-12-20 05:38:17 -0800

    implement a function that allows a user to deposit funds

--- Blockchain Dev. Bootcamp/SendMoneyExample.sol
+++ Blockchain Dev. Bootcamp/SendMoneyExample.sol
@@ -1,3 +1,11 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.14;
+pragma solidity 0.8.15;
+
+contract SendWithdrawMoney {
+
+    uint public balanceReceived;
+    function deposit() public payable {
+         balanceReceived += msg.value;
+    }
+}
