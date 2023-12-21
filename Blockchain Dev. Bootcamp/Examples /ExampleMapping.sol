fbd9ab6ea91550d1c676b495d80092d655f932a3
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2023-12-21 05:40:46 -0800

    an exampke of how a mapping can be used for an address

--- Blockchain Dev. Bootcamp/Examples /ExampleMapping.sol
+++ Blockchain Dev. Bootcamp/Examples /ExampleMapping.sol
@@ -5,12 +5,19 @@ pragma solidity 0.8.15;
 contract ExampleMapping {
 
     //This works very much like an array - with all uint values assigned the default value of the boolean
-// i.e false
+    // i.e false
     mapping(uint => bool) public myMapping;
+
+    // A useful case for this for addresses is whitelisting, or having a maximum withdraw amount, for example
+    mapping(address => bool) public myAddressMapping;
 
     //A function to take a uint and change its value to true
     function setValue(uint _index) public {
         myMapping[_index] = true;
+    }
+
+    function setMyAddressToTrue() public {
+        myAddressMapping[msg.sender] = true;
     }
 
 }
