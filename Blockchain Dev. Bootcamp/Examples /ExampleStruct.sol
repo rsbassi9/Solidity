f86ed5e2ba304a0bbe64f30c390bc19f45716413
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-03 05:02:05 -0800

    An example of how a payment tracks a value and address of a payment without a struct

--- Blockchain Dev. Bootcamp/Examples /ExampleStruct.sol
+++ Blockchain Dev. Bootcamp/Examples /ExampleStruct.sol
@@ -1,3 +1,22 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.15;
+
+contract wallet {
+
+    PaymentReceived public payment;
+
+    function payContract() public payable {
+        payment = new PaymentReceived(msg.sender, msg.value);
+    }
+}
+
+contract PaymentReceived {
+    address public from;
+    uint public amount;
+
+    constructor(address _from, uint _amount) {
+        from = _from;
+        amount = _amount;
+    }
+}
