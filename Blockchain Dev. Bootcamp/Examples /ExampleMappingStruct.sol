103978805d85971358d4a395675670628b70ffe3
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-03 05:43:28 -0800

    initialize the struct

--- Blockchain Dev. Bootcamp/Examples /ExampleMappingStruct.sol
+++ Blockchain Dev. Bootcamp/Examples /ExampleMappingStruct.sol
@@ -5,7 +5,26 @@ pragma solidity 0.8.15;
 // Below, we create separate functions to send and withdraw monet
 // But this doesnt track a lot, unless you look at a block explorer
 contract MappingStructExample {
-    mapping(address => uint) balance;
+
+        // instead of  something like this: mapping(address => uint) balance;
+      //use a struct to record  a transaction:
+    struct Transaction {
+        uint amount;
+        uint timestamp;
+    }
+
+    // use another struct to keep the balance instead of using a simple unsigned integer
+        // Here we track the current balance, number of deposits, actual deposits, number of withdrawals and the actual withdrawals
+    struct Balance {
+        uint totalBalance;
+        uint numDeposits;
+        //a mapping to track the transaction for each deposit
+        mapping(uint => Transaction) deposits;
+        uint numwithdrawals;
+        //a mapping to track the transaction for each withdrawal
+        mapping(uint => Transaction) withdrawals;
+    }
+
 
     function depositMoney() public payable {
         balance[msg.sender] += msg.value;
