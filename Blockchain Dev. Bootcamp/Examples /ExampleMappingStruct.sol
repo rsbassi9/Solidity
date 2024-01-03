823d6a3fd46b13e3fa0ed107fef4b965e62d003c
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-03 05:34:34 -0800

    showcase how code would look for an untracked bit of code, without a struct

--- Blockchain Dev. Bootcamp/Examples /ExampleMappingStruct.sol
+++ Blockchain Dev. Bootcamp/Examples /ExampleMappingStruct.sol
@@ -2,3 +2,18 @@
 
 pragma solidity 0.8.15;
 
+// Below, we create separate functions to send and withdraw monet
+// But this doesnt track a lot, unless you look at a block explorer
+contract MappingStructExample {
+    mapping(address => uint) balance;
+
+    function depositMoney() public payable {
+        balance[msg.sender] += msg.value;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public {
+        balance[msg.sender] -= _amount;
+
+        _to.transfer(_amount);
+    }
+}
