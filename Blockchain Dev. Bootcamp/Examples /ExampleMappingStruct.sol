c10d2b940690b272b95afb6e0448692065430bc9
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-03 06:40:34 -0800

    added the same functionality to withdrawals

--- Blockchain Dev. Bootcamp/Examples /ExampleMappingStruct.sol
+++ Blockchain Dev. Bootcamp/Examples /ExampleMappingStruct.sol
@@ -20,7 +20,7 @@ contract MappingStructExample {
         uint numDeposits;
         //a mapping to track the transaction for each deposit
         mapping(uint => Transaction) deposits;
-        uint numwithdrawals;
+        uint numWithdrawals;
         //a mapping to track the transaction for each withdrawal
         mapping(uint => Transaction) withdrawals;
     }
@@ -31,19 +31,24 @@ contract MappingStructExample {
 
     function depositMoney() public payable {
        // Now, we can replace: balance[msg.sender] += msg.value;
-    // We can access vatiabloes from our struct using "." . Below, we want to increase out totalBalance by the msg.value, but also want to record a new transaction
+    // We can access variables from our struct using "." . Below, we want to increase out totalBalance by the msg.value, but also want to record a new transaction
        balances[msg.sender].totalBalance += msg.value;
 
        Transaction memory deposit = Transaction(msg.value, block.timestamp);
        // now place this transaction on the current position of deposits (i.e, iots an array of [0,1,...], and then increment the number of deposits
        // So, for the current address that deposits, we get the deposits, access the number of deposits and assign it as the deposit
        balances[msg.sender].deposits[balances[msg.sender].numDeposits] = deposit;
-       balances[msg.sender].numdeposits++;
+       balances[msg.sender].numDeposits++;
        
     }
 
+    // Do the same for withdrawals
     function withdrawMoney(address payable _to, uint _amount) public {
-        balance[msg.sender] -= _amount;
+        balances[msg.sender].totalBalance-= _amount;
+
+        Transaction memory withdrawal = Transaction(_amount, block.timestamp);
+        balances[msg.sender].withdrawals[balances[msg.sender].numWithdrawals] = withdrawal;
+        balances[msg.sender].numWithdrawals++;
 
         _to.transfer(_amount);
     }
