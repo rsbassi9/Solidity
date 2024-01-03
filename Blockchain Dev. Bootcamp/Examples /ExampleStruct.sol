7ea94469dad898fcf2fc32379ec320106e12b1bb
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-03 05:22:04 -0800

    Added detailed notes

--- Blockchain Dev. Bootcamp/Examples /ExampleStruct.sol
+++ Blockchain Dev. Bootcamp/Examples /ExampleStruct.sol
@@ -2,7 +2,11 @@
 
 pragma solidity 0.8.15;
 
-contract wallet {
+// If you deploy the "Wallet" smart contract and send 1 wei to the payContract function (1) , it will use up 221530 gas (2). Why? Because it deploys a new contract "PaymentReceived", then links it.
+// If you now want to interact with the contract, you actually need to tell Remix where to find its address (3) and then interact with it because its a normal contract (4).
+
+// Now,  this is the "normal" way for everyone who is coming from an object oriented language out there. And there are good reasons to do so normally, but with Solditiy you are facing the challenge of limited resources
+contract Wallet {
 
     PaymentReceived public payment;
 
@@ -27,6 +31,11 @@ contract PaymentReceived {
             // b) Complexity.
             // c) to prevent repetition and increase simplicity
 
+ //Let's change the Smart Contract above to use a struct instead. A struct is a way to generate a new DataType, by basically grouping several simple Data Types together - or group groups of simple types together, like a mapping.
+
+//If you interact with the contract now, and you send the same 1 wei to the payContract function you will witness two things:
+    //It suddenly costs only 75394 gas (almost 3 times less!)
+    //You can directly interact with the payment public getter function without interacting with a separate contract.
 contract wallet2 {
 
     struct PaymentReceivedStruct {
@@ -45,3 +54,29 @@ contract wallet2 {
        // payment.amount = msg.value;
     }
 }
+
+/*
+
+If you come from an object oriented programming language, you are normally used to declare a new class for logical group of variables.
+
+In solidity you could define a new contract and give it certain properties, but usually that's extremely gas inefficient.
+
+Solidity uses structs to define new datatypes and group several variables together.
+
+
+What is better: Solidity struct vs Child Contract?¶
+Most of the time, its better to save on gas costs and use structs. This can also include very complex data structures as you will see later.
+
+Good reasons to use Structs over Child Contract:
+
+-Saving Gas Costs! Deploying Child Contracts is simply more expensive.
+-Saving Compexity: Every time you need to access a child contracts property or variable, it needs to go through the child contracts address. For structs, internally, that's just an keccak hash for a lookup at the storage location.
+-DRY/KISS principle (Don't repeat yourself, keep is simple, stupid): Contracts are code. Every time you deploy the same contract with the same code you basically repeated yourself. And its not really simple either.
+-Attack Vector: Contracts are running on its own address. If you have a set of getter and setter functions, there is a chance you need to have access lists or permissions that need to be managed separately.
+
+Having said all this, there are also good reasons for using Child Contracts over Structs:
+-Contract can have any code you want and that means the power of composability. You can put the contracts together like lego pieces and each contrac can have its own logic built in.
+-Smaller Contracts if the code is split across several contracts. In the particular example above, you don't need to get payments from the wallet contract, you can get the payments from the child contract.
+-Access: Contracts have their own address, can have their own data structures and their own interfaces.
+
+*/
