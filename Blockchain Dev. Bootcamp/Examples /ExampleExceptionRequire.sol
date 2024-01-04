c90931664f0180fcc8e9e0f1d73886198a68d6bb
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-04 05:32:35 -0800

    changed the if statement to a require statement for better user experience

--- Blockchain Dev. Bootcamp/Examples /ExampleExceptionRequire.sol
+++ Blockchain Dev. Bootcamp/Examples /ExampleExceptionRequire.sol
@@ -11,13 +11,25 @@ contract ExampleExceptionsRequire {
     }
 
     function withdrawMoney(address payable _to, uint _amount) public {
-        
-        // check if the amount is less than the balance received
+
+       /*  Below, if the amount withdrawn entered by the user is greater than the balance received, then the transaction simply doesnt go through
+        // check if the amount being sent is less than the balance received
         if(_amount <= balanceReceived[msg.sender]) {
             //decerease the balance Received by the amount
             balanceReceived[msg.sender] -= _amount;
             // transfer the amount
             _to.transfer(_amount);
         }
+        */
+
+        // In order to provide the user with a better experience, we want them to know why it failed. here we use require:
+        // If the first argument is false, the entire transaction is rolled bakc, and the error message is displayed
+        require(_amount <= balanceReceived[msg.sender], "Not enough funds, aborting!") {
+            //decerease the balance Received by the amount
+            balanceReceived[msg.sender] -= _amount;
+            // transfer the amount
+            _to.transfer(_amount);
+        }
+    
     }
 }
