92583a64523b02fc6f4f926b589160148e3d4c7f
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-08 05:25:30 -0800

    Create a function that inevitably throws an error

--- Blockchain Dev. Bootcamp/Examples /ExampleTryCatch.sol
+++ Blockchain Dev. Bootcamp/Examples /ExampleTryCatch.sol
@@ -1,3 +1,10 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.15;
+
+contract WillThrow {
+    //a function that inevitably throws an error message
+    function aFunction() public pure{
+        require(false, "Error Message");
+    }
+}
