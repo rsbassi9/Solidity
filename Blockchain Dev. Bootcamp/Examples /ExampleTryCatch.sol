601b90519874541b4d95854272aafc94a5be9b5d
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-08 05:30:09 -0800

    write another contract to handle the error

--- Blockchain Dev. Bootcamp/Examples /ExampleTryCatch.sol
+++ Blockchain Dev. Bootcamp/Examples /ExampleTryCatch.sol
@@ -8,3 +8,17 @@ contract WillThrow {
         require(false, "Error Message");
     }
 }
+
+// To catch this: but it needs to be caught from within another smart contract
+contract ErrorHandling {
+    //create an event to log the error. We wll come back to events later.
+    event ErrorLogging(string reason)
+;    function catchTheError() public {
+        WillThrow will = new WillThrow();
+        try will.aFunction() {
+            // if it works, add code here
+        } catch Error(string memory reason) {
+            emit ErrorLogging(reason);
+        }
+    }
+}
