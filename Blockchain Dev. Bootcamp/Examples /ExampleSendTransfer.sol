3e8f1b9d22bbaece1a07985824e977d64ba4676d
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-09 04:56:03 -0800

    wrote a transfer and a send function

--- Blockchain Dev. Bootcamp/Examples /ExampleSendTransfer.sol
+++ Blockchain Dev. Bootcamp/Examples /ExampleSendTransfer.sol
@@ -1,3 +1,18 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.23;
+
+contract Sender {
+
+// A callback function to receive funds
+    receive() external payable {}
+
+// Both the functions below send 10 Wei to the _to address. So which one should you use in a scenario, and why?
+    function withdrawTransfer(address payable _to) public {
+        _to.transfer(10);
+    }
+
+    function withdrawSend(address payable _to) public {
+        _to.send(10);
+    }
+}
