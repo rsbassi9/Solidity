4b5164ade1be106c2cde2eed89812f4d5a31bc33
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-09 06:14:08 -0800

    added specificity to contractTwo to show how to send to a contract that we are not sure if it is a smart contract

--- Blockchain Dev. Bootcamp/Examples /ExampleContractCall.sol
+++ Blockchain Dev. Bootcamp/Examples /ExampleContractCall.sol
@@ -15,10 +15,17 @@ contract ContractTwo {
     receive() external payable {}
 
     function depositOnContractOne(address _contractOne) public {
-        ContractOne one = ContractOne(_contractOne);
+        // What if we dont know that the smart contract we want to send to is a smart contract?
+        // Use the encodeEithSignature function
+        bytes memory payload = abi.encodeWithSignature("depost()");
+        (bool success, )_contractOne.{value: 10, gas: 100000}(payload);
+        require (success);
+
+        
         // To make sure that the value specified in deposit actually goes to the addressBalances, use syntax {}
         // and specify a higher gas. send and transfer only allocate 2300gas to be executed. This is far too little. 
         // We can specify higher gas allocation to ensure the funds are sent
-        one.deposit{value: 10, gas: 100000}();
+        /*ContractOne one = ContractOne(_contractOne);
+        one.deposit{value: 10, gas: 100000}();*/
     }
 }
