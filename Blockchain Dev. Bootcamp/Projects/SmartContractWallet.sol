0239a0d1f638edda6ea06ebf0a3b7026314e25a6
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-10 06:06:38 -0800

    allow transfers to occur

--- Blockchain Dev. Bootcamp/Projects/SmartContractWallet.sol
+++ Blockchain Dev. Bootcamp/Projects/SmartContractWallet.sol
@@ -11,6 +11,15 @@ address payable owner;
         owner = payable(msg.sender);
     }
 
+    // Transferrability of funds
+    function transfer(address payable _to, uint _amount, bytes memory _payload) public returns(bytes memory) {
+        require(msg.sender == owner, "You are not the owner, aborting!");
+
+        (bool success, bytes memory returnData) = _to.call{value: _amount}(_payload);
+        require(success, "Aborting, call was not successful");
+        return returnData;
+    }
+
     // Allow the waller to receiver funds no matter what
     receive() external payable {}
 
