c4a38bf032bbd293c751bc8cb86dc100fecc155b
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-10 06:05:32 -0800

    set the owner of teh wallet

--- Blockchain Dev. Bootcamp/Projects/SmartContractWallet.sol
+++ Blockchain Dev. Bootcamp/Projects/SmartContractWallet.sol
@@ -1,3 +1,10 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.16;
+
+address payable owner;
+
+    // Set the owner of the wallet
+    constructor() {
+        owner = payable(msg.sender);
+    }
