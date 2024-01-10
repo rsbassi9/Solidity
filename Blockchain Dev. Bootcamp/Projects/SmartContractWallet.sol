90a93c773a6bd2e29f19c0fbb21f58e99da0a336
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-10 06:23:56 -0800

    update checkes

--- Blockchain Dev. Bootcamp/Projects/SmartContractWallet.sol
+++ Blockchain Dev. Bootcamp/Projects/SmartContractWallet.sol
@@ -4,23 +4,48 @@ pragma solidity 0.8.16;
 
 contract SmartContractWallet {
 
-address payable owner;
+    address payable owner;
+
+    // Allowance functionality to spend a certain amount of funds
+    mapping(address => uint) public allowance;
+    mapping(address => bool) public isAllowedToSend;
 
     // Set the owner of the wallet
     constructor() {
         owner = payable(msg.sender);
     }
 
+    // Setter function to prevent sending if you are not allowed to
+    function setAllowance(address _for, uint _amount) public {
+        require(msg.sender == owner, "You are not the owner, aborting!");
+        allowance[msg.sender] = _amount;
+
+        if(_amount > 0) {
+            isAllowedToSend[_for] = true;
+        } else {
+            isAllowedToSend[_for] = false;
+        }
+    }
+
     // Transferrability of funds
     function transfer(address payable _to, uint _amount, bytes memory _payload) public returns(bytes memory) {
-        require(msg.sender == owner, "You are not the owner, aborting!");
+        //require(msg.sender == owner, "You are not the owner, aborting!");
+        if(msg.sender != owner) {
+            require(allowance[msg.sender] >= _amount, "You are trying to send more than you are allowed, aborting");
+            require(isAllowedToSend[msg.sender], "You are not allowed to send anything from this smart contract, aborting");
+
+            //reduce teh allowance once sent
+            allowance[msg.sender] -= _amount;
+        }
 
         (bool success, bytes memory returnData) = _to.call{value: _amount}(_payload);
         require(success, "Aborting, call was not successful");
         return returnData;
     }
 
-    // Allow the waller to receiver funds no matter what
+    // Allow the wallet to receiver funds no matter what
     receive() external payable {}
+
+    
 
 }
