d61c4efb6a0897cbea94be52ca85b76fe914be46
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-10 06:06:12 -0800

    allo funds to be received

--- Blockchain Dev. Bootcamp/Projects/SmartContractWallet.sol
+++ Blockchain Dev. Bootcamp/Projects/SmartContractWallet.sol
@@ -2,9 +2,16 @@
 
 pragma solidity 0.8.16;
 
+contract SmartContractWallet {
+
 address payable owner;
 
     // Set the owner of the wallet
     constructor() {
         owner = payable(msg.sender);
     }
+
+    // Allow the waller to receiver funds no matter what
+    receive() external payable {}
+
+}
