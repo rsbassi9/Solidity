5ecb8d76d98000a3d7705b525ead7869c1c997e1
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-15 04:56:42 -0800

    Created the ability for a function to becalled to create guardians of the wallet so that in case the private key is lost, they are able to assign a new owner and recover the funds

--- Blockchain Dev. Bootcamp/Projects/SmartContractWallet.sol
+++ Blockchain Dev. Bootcamp/Projects/SmartContractWallet.sol
@@ -10,9 +10,18 @@ contract SmartContractWallet {
     mapping(address => uint) public allowance;
     mapping(address => bool) public isAllowedToSend;
 
+    //Set the owner to a different address by a minimum of 3 guardians in case the funds are lost (e.g, the owner loses their private key)
+    mapping(address => bool) public guardians;
+
     // Set the owner of the wallet
     constructor() {
         owner = payable(msg.sender);
+    }
+
+    // Make the guardians
+    function setGuardian(address _guardian, bool _isGuardian) public {
+        require(msg.sender == owner, "You are not the owner, aborting!");
+        guardians[_guardian] = _isGuardian;
     }
 
     // Setter function to prevent sending if you are not allowed to spend any funds
