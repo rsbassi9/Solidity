304b6bcca127560471ada86932f4ff29af595ee7
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-16 05:12:26 -0800

    Added notes and instructions to run the contract an script

--- Blockchain Dev. Bootcamp/web3.js/sampleDebugging.sol
+++ Blockchain Dev. Bootcamp/web3.js/sampleDebugging.sol
@@ -60,3 +60,43 @@ contract MyContract {
 })()
 
  */
+
+
+/*
+NOTES:
+
+ABI stands for Application Binary Interface.
+
+When you compile a smart contract then you get bytecode which gets deployed. Inside there are assembly opcodes telling the EVM during execution of a smart contract where to jump to. Those jump destinations are the first 4 bytes of the keccak hash of the function signature.
+
+Best is, we're looking through the lens of the debugger into the execution of a smart contract and run through that.
+
+INSTRUCTIONS:
+In Remix, go to the Plugins and enable the Debugger plugin.
+Deploy the Smart Contract
+update the myUint through setMyUint
+Then hit the "debug" button in the console window
+The debugger will open up. On the left side, you see a horizontal scrollbar. Scroll it all the way to the beginning.
+If you scroll (or step into) until instruction 041, you should see something like PUSH4 e492fd84, which means, its now pushing the 4 bytes e492fd84 onto the stack and then in 046 its comparing it to the first 4 bytes of the calldata, which, incidentally also contains e492fd84 as the first 4 bytes.
+
+Where are they coming from?
+
+It's the first 4 bytes of the function signature bytes4(keccak256(setMyUint(uint256))).
+
+As you know with hashes, once you hashed a value its hard to go back, unless you have a rainbow table or do brute forcing of some sorts.
+
+And web3js tries to offer nice functions to interact with a smart contract. So, in web3js, you can do things like contractInstance.methods.setMyUint(123), all with auto-completion. That's only possible, if we provide the information what functions exist on a smart contract to web3js.
+
+This is what the ABI array does.
+
+The ABI Array contains all functions, inputs and outputs, as well as all variables and their types from a smart contract.
+
+If you open up the artifacts/MyContract.json file in the file-explorer, you can scroll all the way to the bottom. There you find the ABI array:
+
+If you want to interact with a smart contract from web3js, you need two things:
+
+the ABI array
+the contract address (or you deploy the contract through web3js)
+Run the web3js script above.
+Then right-click on the file and run the script.
+ */
