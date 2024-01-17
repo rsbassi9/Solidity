401cb22c7cbc6f4ab7127fa8e7db1b72c8daa76a
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-17 04:57:42 -0800

    create functions to burn and create new tokens by the owner of teh contract

--- Blockchain Dev. Bootcamp/Examples /ExampleModifiers.sol
+++ Blockchain Dev. Bootcamp/Examples /ExampleModifiers.sol
@@ -1,3 +1,29 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.16;
+
+contract InheritanceModifierExample{
+
+    mapping(address => uint) public tokenbalance;
+
+    address owner;
+
+    uint tokenPrice = 1 ether;
+
+    constructor() {
+        owner = msg.sender;
+        tokenBalance[owner] = 100;
+    }
+
+// Only the owner can create and burn tokens
+    function createNewToken() public {
+        require(msg.sender == owner, "You are not allowed");
+        tokenBalance[owner]++;
+    }
+
+    function burnToken() public {
+        require(msg.sender == owner, "You are not allowed");
+        tokenBalance[owner]--;
+    }
+
+}
