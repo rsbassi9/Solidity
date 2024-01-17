d4ef419336978672843b4bb5f4815b1a804a70fb
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-17 05:56:52 -0800

    Explained virtual and override keywords

--- Blockchain Dev. Bootcamp/Examples /InheritanceModifier/Ownable.sol
+++ Blockchain Dev. Bootcamp/Examples /InheritanceModifier/Ownable.sol
@@ -16,3 +16,17 @@ contract Owner{
         _;
     }
 }
+
+/*
+"Virtual" and "override":
+The base contract should have the name "virtual" to indicate that it can be re-written in any other contracts that inherit from it
+The base function should have the name "override" if it is taking base functionality from a base contract and is planning to be able to override it.
+
+If a function has both, it could be an intermediary contract, i.e, inherits from a base contract and 
+has the ability to override the base function(override keyword), but also in itself can be overriden when used in another smart contract (virtual keyword)
+
+function name() public view virtual ovveride returns (string memory){
+    return _name;
+}
+
+ */
