d42827708a92711f28cd01130509fc516aa42a61
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-17 06:15:17 -0800

    updated documentation

--- Blockchain Dev. Bootcamp/Examples /sampleUnits.sol
+++ Blockchain Dev. Bootcamp/Examples /sampleUnits.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.16;
 
 // Since currency is denominated in Wei, ratehr than writing 1e18 and 2e18,
-// A better way to write those units is to internally denominate in ether:
+// A better way to write those units is to internally denominate in ether: (other units: wei, gwei, ether)
 contract SampleUnits {
     modifier betweenOneandTwoEth() {
         require(msg.value <= 1 ether && msg.value <= 2 ether);
@@ -14,7 +14,7 @@ contract SampleUnits {
     uint runUntilTimestamp;
     uint startTimestamp;
 
-    // in Solidity, timestamps are in unix (seconds) by default, but theres a better way to write them:
+    // in Solidity, timestamps are in unix (seconds) by default, but theres a better way to write them: (other units: seconds, minutes, hours, days, weeks)
         // startTimestamp = block.timestamp + (startInDays * 24 * 60 * 60);
         //runUntilTimestamp = startTimestamp + (7 * 24* 60 * 60); 
     constructor(uint startInDays) {
