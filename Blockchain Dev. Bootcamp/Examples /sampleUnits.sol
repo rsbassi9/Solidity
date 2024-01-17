06641d66a8c9f9e5ee805a9abd5d012cfb8f2a51
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-17 06:07:49 -0800

    showcased a better way toi denominate currency

--- Blockchain Dev. Bootcamp/Examples /sampleUnits.sol
+++ Blockchain Dev. Bootcamp/Examples /sampleUnits.sol
@@ -2,6 +2,11 @@
 
 pragma solidity ^0.8.16;
 
+// Since currency is denominated in Wei, ratehr than writing 1e18 and 2e18,
+// A better way to write those units is to internally denominate in ether:
 contract SampleUnits {
-    
+    modifier betweenOneandTwoEth() {
+        require(msg.value <= 1 ether && msg.value <= 2 ether);
+        _;
+    }
 }
