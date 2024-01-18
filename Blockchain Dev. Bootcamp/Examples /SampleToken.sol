58f0cb6de3d7ba388fd9fdc6d1fd1df4f2b0afbb
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-18 06:28:46 -0800

    A function to allow someoen to buy on your behalf

--- Blockchain Dev. Bootcamp/Examples /SampleToken.sol
+++ Blockchain Dev. Bootcamp/Examples /SampleToken.sol
@@ -10,7 +10,7 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 contract CoffeeToken is ERC20, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    event CoffeePurchased(address indexed receiver);
+    event CoffeePurchased(address indexed receiver, address indexed buyer);
 
     constructor(address defaultAdmin, address minter) ERC20("CoffeeToken", "CFE") {
         _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
@@ -23,8 +23,15 @@ contract CoffeeToken is ERC20, AccessControl {
     }
 
     // Create the ability to use tokens to buy a coffee
-    function burOneCoffee() public {
+    function buyOneCoffee() public {
         _burn(_msgSender(), 1);
         emit CoffeePurchased(_msgSender());
+    }
+
+    // A person can give other people the allowance to buy coffees in his name
+    function buyOneCoffeeFrom(address account) public {
+       _spendAllowance(account, _msgSender(), 1); 
+       _burn(account, 1);
+       emit CoffeePurchased(_msgSender(), account);
     }
 }
