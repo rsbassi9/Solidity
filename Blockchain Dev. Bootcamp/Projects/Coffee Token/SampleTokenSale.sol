0353a99819fd884df01de68fb6d6641a35708653
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-19 05:17:24 -0800

    implement the token sale contract

--- Blockchain Dev. Bootcamp/Projects/Coffee Token/SampleTokenSale.sol
+++ Blockchain Dev. Bootcamp/Projects/Coffee Token/SampleTokenSale.sol
@@ -6,4 +6,28 @@ pragma solidity ^0.8.20;
 abstract contract ERC20 {
     function transferFrom(address _from, address _to, uint256 _value) public virtual returns (bool success);
     function decimals() public virtual view returns (uint8);
+
+    contract TokenSale {
+
+    uint public tokenPriceInWei = 1 ether;
+
+    ERC20 public token;
+    address tokenOwner;
+
+    constructor(address _token){
+        tokenOwner = msg.sender;
+        token = ERC20(_token);
+    } 
+
+    function pusrchaseACoffee() public payable {
+        require(msg.value >= tokenPriceInWei, "Not enough money sent");
+
+
+        // Only allow sending of whole numbers. if a partial token is send, eg 1.5 ETH, send back 0.5 ETH
+        uint tokensToTransfer = msg.value / tokenPriceInWei;
+        uint remainder = msg.value - tokensToTransfer * tokenPriceInWei;
+        token.transferFrom(tokenOwner, msg.sender, tokensToTransfer);
+        payable(msg.sender).transfer(remainder);
+    }
+
 }
