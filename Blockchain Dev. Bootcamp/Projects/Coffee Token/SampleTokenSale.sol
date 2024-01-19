c18543674bbc90467f5182bdee9f08943130d6f5
rsbassi9 <35208333+rsbassi9@users.noreply.github.com>
2024-01-19 05:10:48 -0800

    Define the token contract

--- Blockchain Dev. Bootcamp/Projects/Coffee Token/SampleTokenSale.sol
+++ Blockchain Dev. Bootcamp/Projects/Coffee Token/SampleTokenSale.sol
@@ -1,2 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
+// We could import our SampleToken contract here in place of the ERC20 contract below
+// https://ethereum.org/en/developers/docs/standards/tokens/erc-20
+abstract contract ERC20 {
+    function transferFrom(address _from, address _to, uint256 _value) public virtual returns (bool success);
+    function decimals() public virtual view returns (uint8);
+}
