a8b5ecf084ed75cce4885b2e49870b529e4f676b
rsbassi9 <rajvir.bassi95@gmail.com>
2024-02-13 06:09:27 -0800

    changed console.log to assert to verify unit test

--- Blockchain_Dev_Bootcamp/NFT/test/Spacebear.test.js
+++ Blockchain_Dev_Bootcamp/NFT/test/Spacebear.test.js
@@ -5,6 +5,12 @@ contract("Spacebear", (accounts) => {
   it("should credit an NFT to a specific account", async () => {
     const spacebearInstance = await Spacebear.deployed();
     await spacebearInstance.safeMint(accounts[1], "spacebear_1.json");
-    console.log(await spacebearInstance.ownerOf(0));
+
+    //instead of using a console.log, we can use the assert.equal to compare the 2 arguments, and return an error message if unequal
+    assert.equal(
+      await spacebearInstance.ownerOf(0),
+      accounts[1],
+      "Owner of Token 1 is not equal to account 2"
+    );
   });
 });
