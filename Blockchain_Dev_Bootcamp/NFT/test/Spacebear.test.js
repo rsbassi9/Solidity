f0cceba526bea2a4f4edc4e9c0afe0d4da5a563e
rsbassi9 <rajvir.bassi95@gmail.com>
2024-02-13 05:58:16 -0800

    create the unit test

--- Blockchain_Dev_Bootcamp/NFT/test/Spacebear.test.js
+++ Blockchain_Dev_Bootcamp/NFT/test/Spacebear.test.js
@@ -1,1 +1,10 @@
 const Spacebear = artifacts.require("Spacebear");
+
+//Truffle has the following function to deploy the contracts, run the migratios on teh test chain, and then give you the accounts
+contract("Spacebear", (accounts) => {
+  it("should credit an NFT to a specific account", async () => {
+    const spacebearInstance = await Spacebear.deployed();
+    await spacebearInstance.safeMint(accounts[1], "spacebear_1.json");
+    console.log(spacebearInstance.ownerOf(0));
+  });
+});
